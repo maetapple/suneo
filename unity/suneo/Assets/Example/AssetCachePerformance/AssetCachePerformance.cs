69748d965321376122c70d33fadbb77a87b9fbfb
maeta <maeta@cloverlab.jp>
2017-07-23 14:39:18 +0900

    [unity] Cache パフォーマンス測定Scene追加

---
+++ unity/suneo/Assets/Example/AssetCachePerformance/AssetCachePerformance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Example
+{
+    public class AssetCachePerformance : MonoBehaviour 
+    {
+        private Suneo.SkeletonDataAssetCacheByString dataAssetCache = null;
+        // private Suneo.SkeletonDataAsset
+
+        void Start () {
+            
+        }
+        
+        // Update is called once per frame
+        void Update () {
+            
+        }
+    }
+}
